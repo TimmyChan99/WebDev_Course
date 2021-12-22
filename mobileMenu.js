05102b06c409087c94f95e998b7e36d671ef3a50
Fatima Ezzahra <felemenoun@gmail.com>
2021-12-22 23:21:23 +0100

    update html and css >> add mobule menu and dynamic html

---
+++ mobileMenu.js
@@ -0,0 +1,11 @@
+/* Mobile Menu */
+
+const navBar = document.querySelector('.navbar_mobile');
+
+function openMenu() {
+    navBar.style.display = 'flex';
+}
+
+function closeMenu() {
+    navBar.style.display = 'none';
+}
